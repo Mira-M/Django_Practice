24968db77348eaa93b72ae7a5a470f6c43f5c13b
Mira-M <mira.mollar@gmail.com>
2015-08-12 14:59:52 -0400

    Practice thus far

---
+++ polls/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import Choice, Question
+
+# Model Registration:
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}), ('Date information', {'fields': ['pub_date']}), ]
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
